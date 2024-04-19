ce86ff0d4d8846e49666184badb64193d0c118c1
moheet333 <moheetshendarkar@gmail.com>
2024-04-19 21:50:33 +0530

    UPDATE: Background set

---
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -0,0 +1,21 @@
+"use client"
+import styled from "styled-components";
+
+export const GradientBackground = styled.div`
+  background: linear-gradient(to right, #000046, #1cb5e0);
+  background-size: 400% 400%;
+  animation: gradient 12s ease infinite;
+  height: 100vh;
+  width: 100vw;
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+`;
